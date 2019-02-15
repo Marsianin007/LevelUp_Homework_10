3a7dca9b50082a9b326afe9fb91d5ebb2dd7a295
Marsianin007 <chernoborodiuk20@gmail.com>
2019-02-15 21:24:20 +0200

    football_team

---
+++ index.js
@@ -0,0 +1,14 @@
+//2 задача
+console.log("Калорийность обеда");
+console.log(320);
+console.log("Калорийность ужина");
+console.log(480);
+
+//3 задача
+console.log('Завтракомер v0.1 запущен');
+
+var milkInGrams = keks.ask('Босс, сколько грамм молока вы отведали?');
+console.log('Завтракомер получил данные о молоке. Выпито ' + milkInGrams + ' грамм.');
+
+var breakfastCalories=milkInGrams*(50/100);
+console.log('Калорийность завтрака: '+breakfastCalories+' калорий, босс!');
